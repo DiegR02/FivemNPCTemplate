b1c7f4ac6dc84fa36fb987e7f72edea538ac93fd
Diego <02diegorf@gmail.com>
2023-01-15 19:02:36 +0100

    fxmanifest.lua and client/client.lua added

---
+++ client/client.lua
@@ -0,0 +1,20 @@
+Sit = {
+    dict = '',
+    anim = ''
+}
+
+Citizen.CreateThread(function()
+    local hash = GetHashKey("mp_m_shopkeep_01")
+    while not HasModelLoaded(hash) do
+    RequestModel(hash)
+    Wait(20)
+    end
+    ped = CreatePed("PED_TYPE_CIVFEMALE", "mp_m_shopkeep_01", 1134.21, -982.39, 45.42, 277.47, false, true)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    FreezeEntityPosition(ped, true)
+    SetEntityInvincible(ped, true)
+    --GiveWeaponToPed(ped, 0x3AABBBAA , 0, true, true])
+    --loadDict(Sit.dict)
+    --TaskPlayAnim(ped, Sit.dict, Sit.anim, 8.0, -8, -1, 1, 0, false, false, false)
+end)
+
---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean' 
+game 'gta5'
+
+description 'A FiveM Non Playable Character (NPC) generator template. + Animations and weapons'
+
+client_script 'client/client.lua'
